be6e021a6af0f89adab1c8df903f3bda9bc6c11f
Ashish <ashishtripa9694@gmail.com>
2026-08-09 22:04:13 +0530

    program

--- EmployeeAnalyticsAPI/EmployeeAnalyticsAPI/Program.cs
+++ EmployeeAnalyticsAPI/EmployeeAnalyticsAPI/Program.cs
@@ -9,11 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddScoped<ISalary, SalaryService>();
-//builder.Services.AddControllers();
-builder.Services.AddControllers().AddJsonOptions(options =>
-{
-    options.JsonSerializerOptions.ReferenceHandler=System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
-});
+builder.Services.AddControllers();
+
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
