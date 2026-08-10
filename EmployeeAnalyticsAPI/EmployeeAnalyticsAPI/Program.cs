0c3951ab9d7d9934320f4f10ba676709dd1f765d
Ashish <ashishtripa9694@gmail.com>
2026-08-10 15:04:28 +0530

    replace the apikey

--- EmployeeAnalyticsAPI/EmployeeAnalyticsAPI/Program.cs
+++ EmployeeAnalyticsAPI/EmployeeAnalyticsAPI/Program.cs
@@ -41,11 +41,8 @@ using (var scop = app.Services.CreateScope())
     await seeder.SeedIfNeededAsync();
 }
 
-    if (app.Environment.IsDevelopment())
-    {
-        app.UseSwagger();
-        app.UseSwaggerUI();
-    }
+app.UseSwagger();
+app.UseSwaggerUI();
 
 
 app.UseHttpsRedirection();
